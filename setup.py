0f84358aa9a98d196dd3e63273ddd73e3ed190e2
kotfu <kotfu@kotfu.net>
2017-08-24 20:06:03 -0600

    only 3.4, 3.5, and 3.6

--- setup.py
+++ setup.py
@@ -46,12 +46,9 @@ setup(
 	'Intended Audience :: Developers',
 	'Intended Audience :: System Administrators',
 	'License :: OSI Approved :: MIT License',
-	'Programming Language :: Python :: 3',
-	'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
 	],
 
 	keywords='java tomcat command line',
