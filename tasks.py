facf2be2684534c516fee9954b8455d95450d3b9
kotfu <kotfu@kotfu.net>
2018-05-15 10:40:19 -0600

    pylint fixes

--- tasks.py
+++ tasks.py
@@ -1,21 +1,18 @@
 #
 # -*- coding: utf-8 -*-
+"""Development related tasks to be run with 'invoke'"""
 
 import os
 import shutil
 
 import invoke
 
-#
-# TODO
-# - figure out how to pass additional command line arguments to the
-#   executed shell command, so you can do '$ invoke pytest --lf'
-
 # shared function
 def rmrf(dirs, verbose=True):
+    "Silently remove a list of directories"
     if isinstance(dirs, str):
         dirs = [dirs]
-    
+
     for dir_ in dirs:
         if verbose:
             print("Removing {}".format(dir_))
@@ -23,43 +20,58 @@ def rmrf(dirs, verbose=True):
 
 
 # create namespaces
-namespace = invoke.Collection() 
+namespace = invoke.Collection()
 namespace_clean = invoke.Collection('clean')
 namespace.add_collection(namespace_clean, 'clean')
 
 #####
 #
-# pytest, tox, and codecov
+# pytest, tox, pylint, and codecov
 #
 #####
 @invoke.task
-def pytest(c):
-    "Run unit and integration tests using pytest"
-    c.run("pytest")
+def pytest(context):
+    "Run tests and code coverage using pytest"
+    context.run("pytest --cov=tomcatmanager")
 namespace.add_task(pytest)
 
 @invoke.task
-def pytest_clean(c):
+def pytest_clean(context):
     "Remove pytest cache directories"
+    #pylint: disable=unused-argument
     dirs = ['.pytest-cache', '.cache']
     rmrf(dirs)
 namespace_clean.add_task(pytest_clean, 'pytest')
 
 @invoke.task
-def tox(c):
+def tox(context):
     "Run unit and integration tests on multiple python versions using tox"
-    c.run("tox")
+    context.run("tox")
 namespace.add_task(tox)
 
 @invoke.task
-def tox_clean(c):
+def tox_clean(context):
     "Remove tox virtualenvs and logs"
+    #pylint: disable=unused-argument
     rmrf('.tox')
 namespace_clean.add_task(tox_clean, 'tox')
 
 @invoke.task
-def codecov_clean(c):
+def pylint(context):
+    "Check code quality using pylint"
+    context.run('pylint --rcfile=tomcatmanager/pylintrc tomcatmanager')
+namespace.add_task(pylint)
+
+@invoke.task
+def pylint_tests(context):
+    "Check code quality of test suite using pylint"
+    context.run('pylint --rcfile=tests/pylintrc tests')
+namespace.add_task(pylint_tests)
+
+@invoke.task
+def codecov_clean(context):
     "Remove code coverage reports"
+    #pylint: disable=unused-argument
     dirs = set()
     for name in os.listdir(os.curdir):
         if name.startswith('.coverage'):
@@ -73,30 +85,31 @@ namespace_clean.add_task(codecov_clean, 'coverage')
 # documentation
 #
 #####
-DOCS_SRCDIR='docs'
-DOCS_BUILDDIR=os.path.join('docs','build')
+DOCS_SRCDIR = 'docs'
+DOCS_BUILDDIR = os.path.join('docs', 'build')
 
 @invoke.task()
-def docs_build(c, builder='html'):
+def docs_build(context, builder='html'):
     "Build documentation using sphinx"
     cmdline = 'python -msphinx -M {} {} {}'.format(builder, DOCS_SRCDIR, DOCS_BUILDDIR)
-    c.run(cmdline)
+    context.run(cmdline)
 namespace.add_task(docs_build, name='docs')
 
 @invoke.task
-def docs_clean(c):
+def docs_clean(context):
     "Remove rendered documentation"
+    #pylint: disable=unused-argument
     rmrf(DOCS_BUILDDIR)
 namespace_clean.add_task(docs_clean, name='docs')
 
 @invoke.task
-def docs_livehtml(c):
+def docs_livehtml(context):
     "Launch webserver on http://localhost:8000 with rendered documentation"
     watch = '-z tomcatmanager -z tests -z .'
     builder = 'html'
     outputdir = os.path.join(DOCS_BUILDDIR, builder)
     cmdline = 'sphinx-autobuild -b {} {} {} {}'.format(builder, DOCS_SRCDIR, outputdir, watch)
-    c.run(cmdline, pty=True)
+    context.run(cmdline, pty=True)
 namespace.add_task(docs_livehtml, name='livehtml')
 
 
@@ -105,24 +118,27 @@ namespace.add_task(docs_livehtml, name='livehtml')
 # build and distribute
 #
 #####
-BUILDDIR='build'
-DISTDIR='dist'
+BUILDDIR = 'build'
+DISTDIR = 'dist'
 
 @invoke.task
-def build_clean(c):
+def build_clean(context):
     "Remove the build directory"
+    #pylint: disable=unused-argument
     rmrf(BUILDDIR)
 namespace_clean.add_task(build_clean, 'build')
 
 @invoke.task
-def dist_clean(c):
+def dist_clean(context):
     "Remove the dist directory"
+    #pylint: disable=unused-argument
     rmrf(DISTDIR)
 namespace_clean.add_task(dist_clean, 'dist')
 
 @invoke.task
-def eggs_clean(c):
+def eggs_clean(context):
     "Remove egg directories"
+    #pylint: disable=unused-argument
     dirs = set()
     dirs.add('.eggs')
     for name in os.listdir(os.curdir):
@@ -134,8 +150,9 @@ def eggs_clean(c):
 namespace_clean.add_task(eggs_clean, 'eggs')
 
 @invoke.task
-def pycache_clean(c):
+def pycache_clean(context):
     "Remove __pycache__ directories"
+    #pylint: disable=unused-argument
     dirs = set()
     for root, dirnames, _ in os.walk(os.curdir):
         if '__pycache__' in dirnames:
@@ -145,27 +162,29 @@ def pycache_clean(c):
 namespace_clean.add_task(pycache_clean, 'pycache')
 
 @invoke.task
-def build_sdist(c):
+def build_sdist(context):
     "Create a source distribution"
-    c.run('python setup.py sdist')
+    context.run('python setup.py sdist')
 namespace.add_task(build_sdist, 'sdist')
 
 @invoke.task
-def build_wheel(c):
+def build_wheel(context):
     "Build a wheel distribution"
-    c.run('python setup.py bdist_wheel')
+    context.run('python setup.py bdist_wheel')
 namespace.add_task(build_wheel, 'wheel')
 
 @invoke.task(pre=[dist_clean, build_clean, build_sdist, build_wheel])
-def build_distribute(c):
+def build_distribute(context):
     "Build and upload a distribution to pypi"
-    c.run('twine upload dist/*')
+    context.run('twine upload dist/*')
 namespace.add_task(build_distribute, 'distribute')
 
 #
 # make a dummy clean task which runs all the tasks in the clean namespace
 clean_tasks = list(namespace_clean.tasks.values())
 @invoke.task(pre=list(namespace_clean.tasks.values()), default=True)
-def clean_all(c):
+def clean_all(context):
+    "Run all clean tasks"
+    #pylint: disable=unused-argument
     pass
 namespace_clean.add_task(clean_all, 'all')
