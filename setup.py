a5e28b9edc65e97f38ab1765f4f93296ff84f6e1
kotfu <kotfu@kotfu.net>
2021-06-12 17:45:46 -0600

    Add py.typed to fix #116

--- setup.py
+++ setup.py
@@ -42,6 +42,10 @@ setup(
     keywords="java tomcat command line",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    # for PEP 561
+    package_data={"tomcatmanager": ['py.typed']},
+    include_package_data=True,
+    #
     python_requires=">=3.6",
     install_requires=[
         "cmd2>=1.0.2,<2",
